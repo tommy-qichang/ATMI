4eb484713434aeb3612ed62ada3464664ed3b34b
unknown <calkufu@hotmail.com>
2019-08-17 09:06:57 +0800

    initial config

--- atmi/webpack.config.js
+++ atmi/webpack.config.js
@@ -64,7 +64,13 @@ module.exports = {
                 ]
             }, {
                 test: /\.css$/,
-                loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+                //loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+                //loader: 'style-loader!css-loader?modules&importLoaders=1'
+                loader: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    //'style-loader'
+                ]
             }
         ]
     },
