2127286cfe58657c32adc3fc8b0531d27b2778dd
qi chang <tommy.qichang@gmail.com>
2020-06-10 00:58:23 -0400

    This version include: 1. Set interval 1s for updating segmentation modification. 2.

--- atmi_backend/db_interface/InitialService.py
+++ atmi_backend/db_interface/InitialService.py
@@ -2,66 +2,26 @@ import sqlite3
 from sqlite3 import Error
 import os
 
+from atmi_backend.db_interface.DbPool import DbPool
+
+db_pool = None
 
 class InitialService:
-    DATABASE_PATH = './atmi_backend/db'
-    SCHEMA_NAME = 'schema.sql'
-    DATABASE_NAME = 'atmi_db'
-    db_connection = None
+
+
+    def __init__(self, pool_num=20):
+        global db_pool
+        if db_pool is None:
+            db_pool = DbPool(count=pool_num)
 
     def get_connection(self):
         """
         singleton instance, will return connection, and will create one if it's not initialized.
         :return: db connection.
         """
-        if InitialService.db_connection is None:
-            InitialService.db_connection = self.initialize_database()
-
-        return InitialService.db_connection
-
-    def close_connection(self):
-        """
-        Close connection if not needed, and set db_connection as None.
-        :return: True
-        """
-        if InitialService.db_connection is not None:
-            InitialService.db_connection.commit()
-            InitialService.db_connection.close()
-            InitialService.db_connection = None
-            return True
-
-        return False
-
-    def initialize_database(self):
-        """
-        Initialize database and setup tables structure.
-        If the database already exists, return cursor without initial table structure.
-        :return:
-        conn: sqlite database connection object
-        """
-
-        try:
-            conn = sqlite3.connect(os.path.join(InitialService.DATABASE_PATH, InitialService.DATABASE_NAME),
-                                   check_same_thread=False)
-            cur = conn.cursor()
-            self.initialize_schema(cur)
-
+        with db_pool as conn:
             return conn
-        except Error as e:
-            # TODO: add log
-            print(e)
-            return None
 
-    def initialize_schema(self, cursor):
-        """
-        Setup table structure
-        :param cursor:
-        :return: True if setup success.
-        """
-        with open(os.path.join(InitialService.DATABASE_PATH, InitialService.SCHEMA_NAME)) as fp:
-            cursor.executescript(fp.read())
-
-        return True
 
     def has_records(self):
         """
